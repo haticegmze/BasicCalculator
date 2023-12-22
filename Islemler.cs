6e94a91a299f6d45f08b2d9031207c7342f04ec0
hatice.korucuk <hatice.korucuk@00N50471>
2023-12-22 10:48:52 +0300

    Add project files.

---
+++ Islemler.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Islemler
+    {
+
+
+        public static double Topla(double sayi1, double sayi2)
+        {
+
+
+            return (sayi1 + sayi2);
+        }
+
+
+        public static int Topla(int sayi1, int sayi2)
+        {
+
+
+
+            return (sayi1 + sayi2);
+        }
+
+
+        public static double Cıkar(double sayi1, double sayi2)
+        {
+
+
+            return (sayi1 - sayi2);
+        }
+
+        public static int Cıkar(int sayi1, int sayi2)
+        {
+
+            return (sayi1 - sayi2);
+        }
+        public static double Bol(double sayi1, double sayi2)
+        {
+
+            return (sayi1 / sayi2);
+
+        }
+
+        public static double Carp(double sayi1, double sayi2)
+        {
+
+            return (sayi1 * sayi2);
+
+        }
+        public static int Carp(int sayi1, int sayi2)
+        {
+            return (sayi1 * sayi2);
+
+        }
+
+        public static double Mod(double sayi1, double sayi2)
+        {
+
+            return (sayi1 % sayi2);
+        }
+
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator
+{
+   public   class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("SONUÇ : " + Islemler.Topla(999,333));
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
